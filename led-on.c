629e7767e6cb4829df0d32e7a2d3047c3adf11c3
Jason McGloin <jason.mcgloin@gmail.com>
2019-06-18 20:59:43 -0400

    defined delay function, coded on/off cycle

--- led-on.c
+++ led-on.c
@@ -7,11 +7,25 @@
 
 
 #include <xc.h>
+#include "led-on.h"
 
+void delay_ms(int limit){
+    for(int i = 1; i <= limit; i++){
+        __delay_ms(1);
+    }
+}
 void main(void) {
     
     TRISB0 = 0;
-    RB0 = 1;
+    int delay = 20;
+    
+    while(1){
+        RB0 = 1;
+        delay_ms(delay);
+        RB0 = 0;
+        delay_ms(delay);
+    }
+    
     
     return;
 }
--- led-on.h
+++ led-on.h
@@ -24,4 +24,4 @@
 
 #include <xc.h>
 
-
+#define _XTAL_FREQ 4000000
